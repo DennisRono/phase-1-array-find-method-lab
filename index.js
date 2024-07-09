bfa7f74c0f3ef26330d30b38cf9564816ea2651c
DennisRono <dennisrkibet@gmail.com>
2024-07-09 14:27:16 +0300

    completed the task asignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(record) {
+  for (let entry of record) {
+    if (entry.result === 'W') {
+      return entry.year
+    }
+  }
+  return undefined
+}
